7dea5c878cd3e7e9e24e3aac1229a551e25310b0
Venky Venky <venkateshsreeramula24@gmail.com>
2021-08-02 16:41:44 +0530

    Thirsty added

--- hello2.js
+++ hello2.js
@@ -5,3 +5,8 @@ console.log('Hi adding a code')
 console.log('Deleting after commit')
 console.log('Deleting after commit')
 console.log('Deleting after commit')
+
+
+console.log('Are u thirsty')
+console.log('Are u thirsty')
+console.log('Are u thirsty')
