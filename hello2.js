7aaff8b859dc0221ca68589a171ed0262d378d0c
Venky Venky <venkateshsreeramula24@gmail.com>
2021-08-02 16:17:18 +0530

    Deleting code after commit

--- hello2.js
+++ hello2.js
@@ -1,3 +1,7 @@
 console.log('Hi I am hello 2 Here')
 
 console.log('Hi adding a code')
+
+console.log('Deleting after commit')
+console.log('Deleting after commit')
+console.log('Deleting after commit')
